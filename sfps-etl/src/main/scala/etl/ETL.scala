e9f0be58a4ffa552ea7c4a09514d3862db331a83
Martin Cura <martincura@gmail.com>
2019-06-02 12:51:07 -0300

    Print parts in a row, sample code

--- sfps-etl/src/main/scala/etl/ETL.scala
+++ sfps-etl/src/main/scala/etl/ETL.scala
@@ -2,6 +2,7 @@ package sfps.etl
 
 import doobie._, doobie.implicits._, doobie.util.ExecutionContexts
 import cats._, cats.data._, cats.effect.IO, cats.implicits._
+import fs2.Stream
 
 object ETL extends App {
 
@@ -18,15 +19,20 @@ object ETL extends App {
     ExecutionContexts.synchronous
   )
 
-  // Debugging only, YOLO mode
-  val y = xa.yolo
-  import y._
+  // YOLO mode, easier for debugging
+  // val y = xa.yolo
+  // import y._
 
-  (sql"SELECT " ++ Fragment.const0(DataSchema.columns) ++ sql" FROM train")
+  val data = (sql"SELECT " ++ Fragment.const0(DataSchema.columns) ++ sql" FROM train")
     .query[(DataSchema.RowId, DataSchema.ActivityFields, DataSchema.TripFields, DataSchema.MaiFields1, DataSchema.MaiFields2, DataSchema.OnlineFields, DataSchema.PaymentFields, DataSchema.OtherFields, DataSchema.Label)]
-    .stream
+    .stream   // fs2.Stream
     .take(1)
-    .quick
-    .unsafeRunSync
+    .compile.toList
+    .transact(xa)
+    .unsafeRunSync  // List of data row tuples
+
+  // I'm not sure this serves as a stream, if it isn't it will probably break
+
+  data.map(_.productIterator.foreach(println))
 
 }
