50e14b5434061e4632deb8d67a76504a2798fcd9
l <lucianasl@gmail.com>
2019-06-03 15:31:25 -0300

    fixes

---
+++ http4s-server/src/main/scala/sfps/http4sserver/Main.scala
@@ -0,0 +1,9 @@
+package sfps.http4sserver
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    Http4sserverServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
