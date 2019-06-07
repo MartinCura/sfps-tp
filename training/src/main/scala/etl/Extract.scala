cc8cc1b6345d5f706d222555a0c533e11388fe48
Martin Cura <martincura@gmail.com>
2019-06-07 17:40:15 -0300

    Organize Extract code to be used as an object from which to take rows

---
+++ training/src/main/scala/etl/Extract.scala
@@ -0,0 +1,59 @@
+package sfps.etl
+
+import doobie._, doobie.implicits._, doobie.util.ExecutionContexts
+import cats._, cats.data._, cats.effect.IO, cats.implicits._
+import fs2.Stream
+import sfps.types._
+
+/**
+ * Sample usage:
+ *
+ * val data = getData(None)
+ * val fiveRows = getNextRows(data, 5)
+ **/
+object ETL extends App {
+  // TODO: move some things to initialization with parameter table name
+
+  // TODO: read DB_NAME from some constants / env file
+  lazy val DB_NAME = "sfps_db"
+
+  private def transactor(): Transactor.Aux[IO,Unit] = {
+    // Create db transactor
+    implicit val cs = IO.contextShift(ExecutionContexts.synchronous)
+    val xa = Transactor.fromDriverManager[IO](
+      "org.postgresql.Driver",
+      s"jdbc:postgresql://localhost:5442/$DB_NAME",
+      "postgres",
+      "",
+      ExecutionContexts.synchronous
+    )
+    return xa
+
+    // YOLO mode, easier for debugging
+    // val y = xa.yolo
+    // import y._
+  }
+
+  private def selectStatement =
+    sql"SELECT " ++ Fragment.const0(Schema.columns) ++ sql" FROM train"
+
+  // private def reducedSelectStatement = sql"SELECT " ++ Fragment.const0(Semigroup.reducedColumns) ++ sql" FROM train"
+
+  def getStream(): Stream[ConnectionIO, Schema.DataRow] =
+    selectStatement.query[Schema.DataRow].stream
+
+  def getData(n: Option[Int]) = n match {
+    case None => getStream().transact(transactor())             // Get a stream for all of it
+    case Some(n) => getStream().take(n).transact(transactor())  // Stop after n elements
+  }
+
+  def getNextRows(data: Stream[IO, Schema.DataRow], n: Int) = data.take(n).compile.toList.unsafeRunSync
+
+  def testRow(): List[MaiScore] = {
+    val xa = transactor()
+    // println(BillingCountryCode(Some("ARG")).toString())
+
+    return sql"SELECT mai_score FROM train".query[MaiScore].stream.take(2).compile.toList.transact(xa).unsafeRunSync
+  }
+
+}
---
+++ training/src/main/scala/saver/PipelineSaver.scala
@@ -0,0 +1,18 @@
+package saver
+
+import java.io.File
+
+import org.apache.spark.ml.PipelineModel
+import org.apache.spark.sql.DataFrame
+import org.jpmml.sparkml.PMMLBuilder
+
+class PipelineSaver {
+
+  def saveToJpmml(pipeline: PipelineModel, schema: DataFrame, path: String ): Unit = {
+
+    val pmml = new PMMLBuilder(schema.schema, pipeline).build
+    new PMMLBuilder(schema.schema, pipeline).buildFile(new File(path))
+
+  }
+
+}
